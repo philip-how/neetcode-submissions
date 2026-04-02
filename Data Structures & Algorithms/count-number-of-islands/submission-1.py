b542825f74a76e18074d09a31aecb8773289a66a
Philip Howard <161447541+philip-how@users.noreply.github.com>
2026-04-02 16:01:11 +1100

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        i = 0
+        islands = 0
+        while i < len(grid):
+            j = 0
+            while j < len(grid[i]):
+                stack = []
+                stack.append((i, j))
+                if grid[i][j] == "1":
+                    islands += 1
+                else:
+                    j += 1
+                    continue
+                while stack:
+                    u, v = stack.pop()
+                    if grid[u][v] == "0":
+                        continue
+                    else:
+                        grid[u][v] = "0"
+                    stack.append((max(u-1,0),v))
+                    stack.append((min(u+1, len(grid) - 1),v))
+                    stack.append((u,max(v-1,0)))
+                    stack.append((u,min(v+1, len(grid[i]) - 1)))
+                j += 1
+            i += 1
+        
+        return islands
+
