16231e6634692ac40b1617388db6550fa6897303
Philip Howard <161447541+philip-how@users.noreply.github.com>
2026-04-06 14:10:52 +1000

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,59 @@
+class Node:
+    def __init__(self, val, exists=False):
+        self.val = val
+        self.children = {}
+        self.exists = exists
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node("")
+
+    def insert(self, word: str) -> None:
+        curr_node = self.root
+
+        i = 0
+        while i < len(word):
+            sofar = word[0:i + 1]
+            if sofar in curr_node.children:
+                curr_node = curr_node.children[sofar]
+            else:
+                new_node = Node(sofar)
+                curr_node.children[sofar] = new_node
+                curr_node = new_node
+            i += 1
+
+        curr_node.exists = True
+
+    def search(self, word: str) -> bool:
+        curr_node = self.root
+        i = 0
+        while i < len(word):
+            sofar = word[0:i + 1]
+            if sofar in curr_node.children:
+                curr_node = curr_node.children[sofar]
+            else:
+                return False
+            i += 1
+        
+        if curr_node.exists:
+            return True
+        else:
+            return False
+
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr_node = self.root
+        i = 0
+        while i < len(prefix):
+            sofar = prefix[0:i + 1]
+            if sofar in curr_node.children:
+                curr_node = curr_node.children[sofar]
+            else:
+                return False
+            i += 1
+        
+        return True
+        
+        
