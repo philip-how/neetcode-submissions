da70c0c3e93958764724214b91540a581c91ae13
Philip Howard <161447541+philip-how@users.noreply.github.com>
2026-04-06 23:00:05 +1000

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+
+    def dfs_srlz(self, root):
+        if root is None:
+            return "nnnnnn"
+        
+        left_index = self.dfs_srlz(root.left)
+        right_index = self.dfs_srlz(root.right)
+
+        my_index = len(self.running_str)
+
+        self.running_str += f"{root.val:06}{left_index:06}{right_index:06}"
+
+        return my_index
+
+    def dfs_desert(self, string, index):
+        me = TreeNode(int(string[index:index+6]))
+        if string[index+6:index+12] != "nnnnnn":
+            left = self.dfs_desert(string, int(string[index+6:index+12]))
+        else:
+            left = None
+        if string[index+12:index+18] != "nnnnnn":
+            right = self.dfs_desert(string, int(string[index+12:index+18]))
+        else:
+            right = None
+        
+        me.left = left
+        me.right = right
+
+        return me
+
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        self.running_str = ""
+        self.dfs_srlz(root)
+
+        return self.running_str
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if len(data) == 0:
+            return None
+        
+        return self.dfs_desert(data, len(data) - 18)
+        
+        
