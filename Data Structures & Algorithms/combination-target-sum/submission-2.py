83d8af0d57401959bdd0c06259702db4ae700b61
Philip Howard <161447541+philip-how@users.noreply.github.com>
2026-04-07 09:13:16 +1000

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def all_possibilities(self, so_far_sum, so_far, nums, target, index):
+        if index < len(nums) - 1:
+            self.all_possibilities(so_far_sum, so_far, nums, target, index + 1)
+        # then consider taking me
+        if target - so_far_sum - nums[index] < 0:
+            return 
+        if target - so_far_sum - nums[index] == 0:
+            so_far.append(nums[index])
+            self.solns.append(so_far.copy())
+            so_far.pop()
+            return
+        so_far.append(nums[index])
+        so_far_sum += nums[index]
+        self.all_possibilities(so_far_sum, so_far, nums, target, index)
+        so_far.pop()
+
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        self.solns = []
+        self.all_possibilities(0, [], nums, target, 0)
+        return self.solns
+        
