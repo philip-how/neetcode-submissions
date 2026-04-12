7514fb609d4c2885866e1f2ce0a03904365f2aa7
Philip Howard <161447541+philip-how@users.noreply.github.com>
2026-04-12 21:41:16 +1000

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        earliest_spot = {}
+        latest_spot = {}
+
+        for i, char in enumerate(s):
+            if char not in earliest_spot:
+                earliest_spot[char] = i
+            latest_spot[char] = i
+        
+        i = 1
+        current_group_size = 1
+        current_group_latest = latest_spot[s[0]]
+        groups = []
+        while i < len(s):
+            if i > current_group_latest:
+                groups.append(current_group_size)
+                current_group_size = 1
+                current_group_latest = latest_spot[s[i]]
+            else:
+                current_group_size += 1
+                current_group_latest = max(current_group_latest, latest_spot[s[i]])
+            i += 1
+        
+        groups.append(current_group_size)
+
+        return groups
